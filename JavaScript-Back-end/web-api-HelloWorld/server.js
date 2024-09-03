4be1b217b184704bea40643a5e913b1453b2db37
Lucas Gabriel <127156538+lcsg7@users.noreply.github.com>
2024-09-02 23:41:29 -0300

    Update server.js

--- JavaScript-Back-end/web-api-HelloWorld/server.js
+++ JavaScript-Back-end/web-api-HelloWorld/server.js
@@ -1,13 +1,1 @@
-const express = require('express');
-const app = express();
 
-// Rota para a página raiz que responde com "Hello World"
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
-// Inicializar o servidor na porta 3000
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
